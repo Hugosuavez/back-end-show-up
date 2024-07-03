9f2b802b69dfbdc1679f381d6037cf7715ca5197
jesjin <jess.hk.jin@gmail.com>
2024-07-03 21:08:47 +0100

    Fix integration tests

--- __tests__/integration-tests.js
+++ __tests__/integration-tests.js
@@ -20,184 +20,6 @@ beforeEach(async () => {
   token = jwt.sign(user, secretKey, { expiresIn: "24h" });
 });
 
-describe('GET /api/entertainers', () => {
-    test('200: responds with an array objects with correct properties', () => {
-        return request(app)
-        .get('/api/entertainers')
-        .expect(200)
-        .then(({body}) => {
-            expect(body.entertainers).toHaveLength(4)
-            body.entertainers.forEach((user) => {
-                expect(user).toMatchObject({
-                    username: expect.any(String),
-                    first_name: expect.any(String),
-                    last_name: expect.any(String),
-                    email: expect.any(String),
-                    profile_img_url: expect.any(String),
-                    user_type: 'Entertainer',
-                    category: expect.any(String),
-                    location: expect.any(String),
-                    entertainer_name: expect.any(String),
-                    description: expect.any(String),
-                    price: expect.any(Number)
-                })
-            })
-        })
-    })
-
-    test('404: route not found', () => {
-        return request(app)
-        .get('/api/nonsense')
-        .expect(404)
-        .then(({body}) => {
-            expect(body.msg).toBe('404: route not found')
-        })
-    })
-})
-
-describe('GET /api/entertainers?location', () => {
-    test('200: returns entertainers in correct location', () => {
-        return request(app)
-        .get('/api/entertainers?location=London')
-        .expect(200)
-        .then(({body}) => {
-            expect(body.entertainers).toHaveLength(3)
-            body.entertainers.forEach((entertainer) => {
-                expect(entertainer).toMatchObject({
-                    username: expect.any(String),
-                    first_name: expect.any(String),
-                    last_name: expect.any(String),
-                    email: expect.any(String),
-                    profile_img_url: expect.any(String),
-                    user_type: 'Entertainer',
-                    category: expect.any(String),
-                    location: 'London',
-                    entertainer_name: expect.any(String),
-                    description: expect.any(String),
-                    price: expect.any(Number)
-                })
-            })
-        })
-    })
-    test('404: invalid location', () => {
-        return request(app)
-        .get('/api/entertainers?location=lanchesterpool')
-        .expect(404)
-        .then(({body}) => {
-            expect(body.msg).toBe("404: Not Found")
-        })
-    })
-})
-
-describe('GET /api/entertainers?category', () => {
-    test('200: returns entertainers in correct category', () => {
-        return request(app)
-        .get('/api/entertainers?category=Juggler')
-        .expect(200)
-        .then(({body}) => {
-            expect(body.entertainers).toHaveLength(2)
-            body.entertainers.forEach((entertainer) => {
-                expect(entertainer).toMatchObject({
-                    username: expect.any(String),
-                    first_name: expect.any(String),
-                    last_name: expect.any(String),
-                    email: expect.any(String),
-                    profile_img_url: expect.any(String),
-                    user_type: 'Entertainer',
-                    category: 'Juggler',
-                    location: expect.any(String),
-                    entertainer_name: expect.any(String),
-                    description: expect.any(String),
-                    price: expect.any(Number),
-                })
-            })
-        })
-    })
-    test('404: invalid category', () => {
-        return request(app)
-        .get('/api/entertainers?category=weapon')
-        .expect(404)
-        .then(({body}) => {
-            expect(body.msg).toBe("404: Not Found")
-        })
-    })
-})
-
-describe('GET /api/entertainers?date', () => {
-    test('200: returns entertainers available on that date', () => {
-        return request(app)
-        .get('/api/entertainers?date=2024-07-01')
-        .expect(200)
-        .then(({body}) => {
-            expect(body.entertainers).toHaveLength(3)
-            body.entertainers.forEach((entertainer) => {
-                expect(entertainer).toMatchObject({
-                    username: expect.any(String),
-                    first_name: expect.any(String),
-                    last_name: expect.any(String),
-                    email: expect.any(String),
-                    profile_img_url: expect.any(String),
-                    user_type: 'Entertainer',
-                    category: expect.any(String),
-                    location: expect.any(String),
-                    entertainer_name: expect.any(String),
-                    description: expect.any(String),
-                    price: expect.any(Number)
-                })
-            })
-        })
-    })
-    test('404: invalid date', () => {
-        return request(app)
-        .get('/api/entertainers?date=chips')
-        .expect(404)
-        .then(({body}) => {
-            expect(body.msg).toBe("404: Not Found")
-        })
-    })
-})
-
-describe('GET /api/entertainers/:user_id', () => {
-    test('200: responds with correct entertainer object', () => {
-        return request(app)
-        .get('/api/entertainers/2')
-        .expect(200)
-        .then(({body}) => {
-            expect(body.entertainer).toMatchObject({
-                user_id: 2,
-                username: expect.any(String),
-                first_name: expect.any(String),
-                last_name: expect.any(String),
-                email: expect.any(String),
-                profile_img_url: expect.any(String),
-                urls: expect.any(Array),
-                user_type: 'Entertainer',
-                category: expect.any(String),
-                location: expect.any(String),
-                entertainer_name: expect.any(String),
-                description: expect.any(String),
-                price: expect.any(Number),
-            })
-        })
-    })
-    test('404: Not Found', () => {
-        return request(app)
-        .get('/api/entertainers/9999')
-        .expect(404)
-        .then(({body}) => {
-            expect(body.msg).toBe("404: Not Found")
-        })
-    })
-    test('400: Bad Request', () => {
-        return request(app)
-        .get('/api/entertainers/biro')
-        .expect(400)
-        .then(({body}) => {
-            expect(body.msg).toBe('400: Bad Request')
-        })
-    })
-})
-
 describe("GET /api/entertainers", () => {
   test("200: responds with an array objects with correct properties", () => {
     return request(app)
@@ -205,8 +27,8 @@ describe("GET /api/entertainers", () => {
       .expect(200)
       .then(({ body }) => {
         expect(body.entertainers).toHaveLength(4);
-        body.entertainers.forEach((entertainer) => {
-          expect(entertainer).toMatchObject({
+        body.entertainers.forEach((user) => {
+          expect(user).toMatchObject({
             username: expect.any(String),
             first_name: expect.any(String),
             last_name: expect.any(String),
@@ -218,10 +40,7 @@ describe("GET /api/entertainers", () => {
             entertainer_name: expect.any(String),
             description: expect.any(String),
             price: expect.any(Number),
-            url: expect.any(String),
-            media_id: expect.any(Number),
           });
-          expect(entertainer).not.toHaveProperty('password');
         });
       });
   });
@@ -256,10 +75,7 @@ describe("GET /api/entertainers?location", () => {
             entertainer_name: expect.any(String),
             description: expect.any(String),
             price: expect.any(Number),
-            url: expect.any(String),
-            media_id: expect.any(Number),
           });
-          expect(entertainer).not.toHaveProperty('password');
         });
       });
   });
@@ -293,10 +109,7 @@ describe("GET /api/entertainers?category", () => {
             entertainer_name: expect.any(String),
             description: expect.any(String),
             price: expect.any(Number),
-            url: expect.any(String),
-            media_id: expect.any(Number),
           });
-          expect(entertainer).not.toHaveProperty('password');
         });
       });
   });
@@ -330,10 +143,189 @@ describe("GET /api/entertainers?date", () => {
             entertainer_name: expect.any(String),
             description: expect.any(String),
             price: expect.any(Number),
-            url: expect.any(String),
-            media_id: expect.any(Number),
           });
-          expect(entertainer).not.toHaveProperty('password');
+        });
+      });
+  });
+  test("404: invalid date", () => {
+    return request(app)
+      .get("/api/entertainers?date=chips")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("404: Not Found");
+      });
+  });
+});
+
+describe("GET /api/entertainers/:user_id", () => {
+  test("200: responds with correct entertainer object", () => {
+    return request(app)
+      .get("/api/entertainers/2")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.entertainer).toMatchObject({
+          user_id: 2,
+          username: expect.any(String),
+          first_name: expect.any(String),
+          last_name: expect.any(String),
+          email: expect.any(String),
+          profile_img_url: expect.any(String),
+          urls: expect.any(Array),
+          user_type: "Entertainer",
+          category: expect.any(String),
+          location: expect.any(String),
+          entertainer_name: expect.any(String),
+          description: expect.any(String),
+          price: expect.any(Number),
+        });
+      });
+  });
+  test("404: Not Found", () => {
+    return request(app)
+      .get("/api/entertainers/9999")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("404: Not Found");
+      });
+  });
+  test("400: Bad Request", () => {
+    return request(app)
+      .get("/api/entertainers/biro")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("400: Bad Request");
+      });
+  });
+});
+
+describe("GET /api/entertainers", () => {
+  test("200: responds with an array objects with correct properties", () => {
+    return request(app)
+      .get("/api/entertainers")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.entertainers).toHaveLength(4);
+        body.entertainers.forEach((entertainer) => {
+          expect(entertainer).toMatchObject({
+            username: expect.any(String),
+            first_name: expect.any(String),
+            last_name: expect.any(String),
+            email: expect.any(String),
+            profile_img_url: expect.any(String),
+            user_type: "Entertainer",
+            category: expect.any(String),
+            location: expect.any(String),
+            entertainer_name: expect.any(String),
+            description: expect.any(String),
+            price: expect.any(Number),
+          });
+          expect(entertainer).not.toHaveProperty("password");
+        });
+      });
+  });
+
+  test("404: route not found", () => {
+    return request(app)
+      .get("/api/nonsense")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("404: route not found");
+      });
+  });
+});
+
+describe("GET /api/entertainers?location", () => {
+  test("200: returns entertainers in correct location", () => {
+    return request(app)
+      .get("/api/entertainers?location=London")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.entertainers).toHaveLength(3);
+        body.entertainers.forEach((entertainer) => {
+          expect(entertainer).toMatchObject({
+            username: expect.any(String),
+            first_name: expect.any(String),
+            last_name: expect.any(String),
+            email: expect.any(String),
+            profile_img_url: expect.any(String),
+            user_type: "Entertainer",
+            category: expect.any(String),
+            location: "London",
+            entertainer_name: expect.any(String),
+            description: expect.any(String),
+            price: expect.any(Number),
+          });
+          expect(entertainer).not.toHaveProperty("password");
+        });
+      });
+  });
+  test("404: invalid location", () => {
+    return request(app)
+      .get("/api/entertainers?location=lanchesterpool")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("404: Not Found");
+      });
+  });
+});
+
+describe("GET /api/entertainers?category", () => {
+  test("200: returns entertainers in correct category", () => {
+    return request(app)
+      .get("/api/entertainers?category=Juggler")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.entertainers).toHaveLength(2);
+        body.entertainers.forEach((entertainer) => {
+          expect(entertainer).toMatchObject({
+            username: expect.any(String),
+            first_name: expect.any(String),
+            last_name: expect.any(String),
+            email: expect.any(String),
+            profile_img_url: expect.any(String),
+            user_type: "Entertainer",
+            category: "Juggler",
+            location: expect.any(String),
+            entertainer_name: expect.any(String),
+            description: expect.any(String),
+            price: expect.any(Number),
+          });
+          expect(entertainer).not.toHaveProperty("password");
+        });
+      });
+  });
+  test("404: invalid category", () => {
+    return request(app)
+      .get("/api/entertainers?category=weapon")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("404: Not Found");
+      });
+  });
+});
+
+describe("GET /api/entertainers?date", () => {
+  test("200: returns entertainers available on that date", () => {
+    return request(app)
+      .get("/api/entertainers?date=2024-07-01")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.entertainers).toHaveLength(3);
+        body.entertainers.forEach((entertainer) => {
+          expect(entertainer).toMatchObject({
+            username: expect.any(String),
+            first_name: expect.any(String),
+            last_name: expect.any(String),
+            email: expect.any(String),
+            profile_img_url: expect.any(String),
+            user_type: "Entertainer",
+            category: expect.any(String),
+            location: expect.any(String),
+            entertainer_name: expect.any(String),
+            description: expect.any(String),
+            price: expect.any(Number),
+          });
+          expect(entertainer).not.toHaveProperty("password");
         });
       });
   });
@@ -366,10 +358,8 @@ describe("GET /api/entertainers/:user_id", () => {
           entertainer_name: expect.any(String),
           description: expect.any(String),
           price: expect.any(Number),
-          url: expect.any(String),
-          media_id: expect.any(Number),
         });
-        expect(body.entertainer).not.toHaveProperty('password');
+        expect(body.entertainer).not.toHaveProperty("password");
       });
   });
   test("404: Not Found", () => {
@@ -430,31 +420,31 @@ describe("GET /api/me", () => {
       .expect(200)
       .then(({ body }) => {
         expect(body).toMatchObject({
-            category: expect.any(String),
-            created_at: expect.any(String),
-            description: expect.any(String),
-            email: expect.any(String),
-            entertainer_name: expect.any(String),
-            first_name: expect.any(String),
-            last_name: expect.any(String),
-            location: expect.any(String),
-            price: expect.any(Number),
-            profile_img_url: expect.any(String),
-            user_id: expect.any(Number),
-            user_type: expect.any(String),
-            username: expect.any(String)
+          category: expect.any(String),
+          created_at: expect.any(String),
+          description: expect.any(String),
+          email: expect.any(String),
+          entertainer_name: expect.any(String),
+          first_name: expect.any(String),
+          last_name: expect.any(String),
+          location: expect.any(String),
+          price: expect.any(Number),
+          profile_img_url: expect.any(String),
+          user_id: expect.any(Number),
+          user_type: expect.any(String),
+          username: expect.any(String),
         });
       });
   });
 });
 
-describe('GET /api', () => {
-    test('200: responds with an object containing descriptions of all other endpoints', () => {
-        return request(app)
-        .get('/api')
-        .expect(200)
-        .then(({body}) => {
-            expect(body.endpoints).toEqual(endpointsData)
-        })
-    })
+describe("GET /api", () => {
+  test("200: responds with an object containing descriptions of all other endpoints", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.endpoints).toEqual(endpointsData);
+      });
+  });
 });
