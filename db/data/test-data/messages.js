ebb8b6bde933edc2ac530eebdef68e26947f8185
jesjin <jess.hk.jin@gmail.com>
2024-07-06 19:17:12 +0100

    update users correctly and testing messages

--- db/data/test-data/messages.js
+++ db/data/test-data/messages.js
@@ -1,30 +1,30 @@
 module.exports = [
   {
-    sender_id: 1,
-    recipient_id: 2,
+    sender_id: 5,
+    recipient_id: 1,
     message: "Looking forward to the show for my girlfriend's birthday!",
   },
   {
-    sender_id: 2,
-    recipient_id: 1,
+    sender_id: 1,
+    recipient_id: 5,
     message:
       "Don't worry about it. It will be super cool show for your girlfriend. Excited to perform for you!",
   },
   {
-    sender_id: 1,
-    recipient_id: 2,
+    sender_id: 5,
+    recipient_id: 1,
     message:
       "Cool!",
   },
   {
-    sender_id: 1,
-    recipient_id: 6,
+    sender_id: 5,
+    recipient_id: 2,
     message:
       "Hi!",
   },
   {
-    sender_id: 6,
-    recipient_id: 1,
+    sender_id: 2,
+    recipient_id: 5,
     message:
       "Hello!",
   },
